db0b8effb0d8f680cce8411fcff51fbe9a6deedc
jdnt <jdnt@lyvc.com>
2012-05-08 10:25:34 +0800

    Signed-off-by: jdnt <jdnt@lyvc.com>

--- gen/co/zsly/R.java
+++ gen/co/zsly/R.java
@@ -18,16 +18,17 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int about=0x7f050007;
+        public static final int about=0x7f050008;
         public static final int app_name=0x7f050000;
-        public static final int com_btn_cancel=0x7f050005;
-        public static final int com_btn_ok=0x7f050004;
-        public static final int com_dialog_title_quit=0x7f050002;
-        public static final int exit=0x7f050006;
-        public static final int navigation=0x7f050008;
-        public static final int returnPage=0x7f050009;
-        public static final int sys_ask_quit_app=0x7f050003;
-        public static final int sys_network_error=0x7f050001;
+        public static final int com_btn_cancel=0x7f050006;
+        public static final int com_btn_ok=0x7f050005;
+        public static final int com_dialog_title_quit=0x7f050003;
+        public static final int exit=0x7f050007;
+        public static final int hello=0x7f050001;
+        public static final int navigation=0x7f050009;
+        public static final int returnPage=0x7f05000a;
+        public static final int sys_ask_quit_app=0x7f050004;
+        public static final int sys_network_error=0x7f050002;
     }
     public static final class xml {
         public static final int cordova=0x7f040000;
